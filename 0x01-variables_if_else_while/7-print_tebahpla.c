47723b90b3fd1cddf28d394eabe3b9a76f9548cb
root <izirenjoel@gmail.com>
2023-09-08 09:33:22 -0700

    smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    char c = 'z';
+	char c = 'z';
 
-    while (c >= 'a')
-    {
-        putchar(c);
-        c--;
-    }
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return 0;
+	return 0;
 }
