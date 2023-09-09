117b3b04f3167425075d459b0258003b3b55f3d1
root <izirenjoel@gmail.com>
2023-09-09 13:51:59 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,27 +1,17 @@
 #include <unistd.h>
 
-/**
- * main - Entry point
- *
- * Description: Prints all single digit numbers of base 10 starting from 0.
- *
- * Return: Always 0 (Success}
- */
-
-#include <unistd.h>
-
 int main(void)
 {
-    int i;
+	int i;
 
-    i = 48;
-    while (i <= 57)
-    {
-        write(1, &i, 1);
-        i++;
-    }
+	i = 48;
+	while (i <= 57)
+	{
+		write(1, &i, 1);
+		i++;
+	}
 
-    write(1, "\n", 1);
+	write(1, "\n", 1);
 
-    return (0);
+	return (0);
 }
