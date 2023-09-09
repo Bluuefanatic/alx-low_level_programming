8aa398f49a0aac839b94e431bcd66fd5a6b815a1
root <izirenjoel@gmail.com>
2023-09-09 13:34:33 -0700

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,30 +5,20 @@
  *
  * Description: Prints all single digit numbers of base 10 starting from 0.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success}
  */
 int main(void)
 {
-    putchar('0');
-    putchar('\n');
-    putchar('1');
-    putchar('\n');
-    putchar('2');
-    putchar('\n');
-    putchar('3');
-    putchar('\n');
-    putchar('4');
-    putchar('\n');
-    putchar('5');
-    putchar('\n');
-    putchar('6');
-    putchar('\n');
-    putchar('7');
-    putchar('\n');
-    putchar('8');
-    putchar('\n');
-    putchar('9');
-    putchar('\n');
+    char digit;
+
+    for (digit = '0'; digit <= '9'; digit++)
+    {
+        putchar(digit);
+        if (digit != '9')
+        {
+            putchar('\n');
+        }
+    }
 
     return 0;
 }
