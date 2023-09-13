90d2f768109ea0ae9b6dcce9ee2df63e349a9019
root <izirenjoel@gmail.com>
2023-09-13 08:22:13 -0700

    Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * is_multiple - Checks if a number is a multiple of 3 or 5
@@ -6,24 +6,30 @@
  *
  * Return: 1 if the number is a multiple, 0 otherwise
  */
-int is_multiple(int n) {
-    return (n % 3 == 0 || n % 5 == 0);
+int is_multiple(int n)
+{
+	return (n % 3 == 0 || n % 5 == 0);
 }
 
 /**
- * sum_multiples - Sums all the multiples of 3 or 5 below 1024
+ * main - Entry point
  *
- * Return: The sum of the multiples
+ * Return: Always 0
  */
-int sum_multiples() {
-    int sum = 0;
-    int i;
+int main(void)
+{
+	int sum = 0;
+	int i;
 
-    for (i = 0; i < 1024; i++) {
-        if (is_multiple(i)) {
-            sum += i;
-        }
-    }
+	for (i = 0; i < 1024; i++)
+	{
+		if (is_multiple(i))
+		{
+			sum += i;
+		}
+	}
 
-    return sum;
+	printf("%d\n", sum);
+
+	return (0);
 }
