563e35b3360a4ff453e41eecf29040a316f476a2
root <izirenjoel@gmail.com>
2023-09-18 15:36:08 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,8 +9,8 @@
 char *generate_password(void)
 {
     char *charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    int charset_length = 62;  // Length of the charset
-    int password_length = 10; // Desired password length
+    int charset_length = 62;  /* Length of the charset */
+    int password_length = 10; /* Desired password length */
     char *password = malloc((password_length + 1) * sizeof(char));
 
     if (password == NULL)
@@ -21,13 +21,14 @@ char *generate_password(void)
 
     srand(time(NULL));
 
-    for (int i = 0; i < password_length; i++)
+    int i;
+    for (i = 0; i < password_length; i++)
     {
         int random_index = rand() % charset_length;
         password[i] = charset[random_index];
     }
 
-    password[password_length] = '\0'; // Null-terminate the string
+    password[password_length] = '\0'; /* Null-terminate the string */
 
     return password;
 }
@@ -38,7 +39,7 @@ int main(void)
 
     printf("%s\n", password);
 
-    free(password); // Remember to free allocated memory
+    free(password); /* Remember to free allocated memory */
 
     return 0;
 }
