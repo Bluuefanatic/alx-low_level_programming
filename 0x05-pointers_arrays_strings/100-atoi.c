1e9375a830816d817f6fc4cf4d226060bd28b16c
root <izirenjoel@gmail.com>
2023-09-18 16:02:51 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <limits.h>
 
 /**
  * _atoi - Converts a string to an integer.
@@ -9,37 +8,20 @@
  */
 int _atoi(char *s)
 {
-    int result = 0;
-    int sign = 1;
-    int i = 0;
+	int result = 0;
+	int sign = 1;
+	int i = 0;
 
-    /* Handle leading spaces and signs */
-    while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
-    {
-        if (s[i] == '-')
-            sign = -sign;
-        i++;
-    }
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+			result = result * 10 + (s[i] - '0');
+		else if (result > 0)
+			break;
+		i++;
+	}
 
-    /* Process digits */
-    while (s[i])
-    {
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            /* Check for overflow */
-            if (result > (INT_MAX - (s[i] - '0')) / 10)
-            {
-                return (sign == 1) ? INT_MAX : INT_MIN;
-            }
-
-            result = result * 10 + (s[i] - '0');
-        }
-        else
-        {
-            break;  /* Break if a non-numeric character is encountered */
-        }
-        i++;
-    }
-
-    return (sign * result);
+	return (sign * result);
 }
