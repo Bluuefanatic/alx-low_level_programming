bc474003bf3d5bbaa768fb6769bb06d32c9ee6e7
root <izirenjoel@gmail.com>
2023-09-18 15:12:18 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,33 +9,37 @@
  */
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-	int i = 0;
+    int result = 0;
+    int sign = 1;
+    int i = 0;
 
-	/* Handle leading spaces and signs */
-	while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
-	{
-		if (s[i] == '-')
-			sign = -sign;
-		i++;
-	}
+    /* Handle leading spaces and signs */
+    while (s[i] == ' ' || s[i] == '-' || s[i] == '+')
+    {
+        if (s[i] == '-')
+            sign = -sign;
+        i++;
+    }
 
-	/* Process digits */
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		/* Check for overflow */
-		if (result > (INT_MAX - (s[i] - '0')) / 10)
-		{
-			if (sign == 1)
-				return (INT_MAX);
-			else
-				return (INT_MIN);
-		}
+    /* Process digits */
+    while (s[i])
+    {
+        if (s[i] >= '0' && s[i] <= '9')
+        {
+            /* Check for overflow */
+            if (result > (INT_MAX - (s[i] - '0')) / 10)
+            {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
 
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
+            result = result * 10 + (s[i] - '0');
+        }
+        else
+        {
+            break;  /* Break if a non-numeric character is encountered */
+        }
+        i++;
+    }
 
-	return (result * sign);
+    return (sign * result);
 }
