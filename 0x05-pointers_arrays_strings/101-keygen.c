28ee41891fefdbb507b5c81fef4bd5d31101497c
root <izirenjoel@gmail.com>
2023-09-18 15:44:59 -0700

    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

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,12 +1,3 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * generate_password - Generates a random valid password.
- * Return: The generated password.
- */
 char *generate_password(void)
 {
     char *charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
