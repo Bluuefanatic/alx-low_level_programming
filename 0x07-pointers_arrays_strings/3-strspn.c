4a18ad72a5c93297614d0f8e43773194602ad750
root <izirenjoel@gmail.com>
2023-09-26 03:07:21 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,22 +9,22 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int isAccepted[256] = {0}; /* Assuming ASCII characters */
+	unsigned int count = 0;
+	int isAccepted[256] = {0}; /* Assuming ASCII characters */
 
-    /* Mark characters in accept as accepted */
-    while (*accept)
-    {
-        isAccepted[(unsigned char)*accept] = 1;
-        accept++;
-    }
+	/* Mark characters in accept as accepted */
+	while (*accept)
+	{
+		isAccepted[(unsigned char)*accept] = 1;
+		accept++;
+	}
 
-    /* Count the number of accepted characters at the beginning of s */
-    while (isAccepted[(unsigned char)*s])
-    {
-        count++;
-        s++;
-    }
+	/* Count the number of accepted characters at the beginning of s */
+	while (isAccepted[(unsigned char)*s])
+	{
+		count++;
+		s++;
+	}
 
-    return (count);
+	return (count);
 }
