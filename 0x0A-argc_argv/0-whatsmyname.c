966a64f45f8d3c59485fb371c29c4feb73e24e3b
root <izirenjoel@gmail.com>
2023-10-02 18:42:59 -0700

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	if (argc > 0) {
+/**
+ * main - Entry point of the program
+ *
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
 		printf("%s\n", argv[0]);
 	}
 	return (0);
