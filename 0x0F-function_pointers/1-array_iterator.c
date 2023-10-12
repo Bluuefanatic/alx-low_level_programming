164eb14047ca229ce2a196ee14d9a6bed1697a43
root <izirenjoel@gmail.com>
2023-10-11 17:01:42 -0700

    If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,10 @@
+/**
+ * array_iterator - Executes a function on each element of an array.
+ * @array: Pointer to the first element of the array.
+ * @size: Number of elements in the array.
+ * @action: Pointer to the function to be executed on each element.
+ */
+
 #include <stddef.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int))
