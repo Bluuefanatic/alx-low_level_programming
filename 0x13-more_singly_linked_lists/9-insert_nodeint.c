7891ccc4708c2ed2194c08f1ed9ec3efd3fd7afe
Bluuefanatic <izirenjoel@gmail.com>
2023-10-24 06:02:30 -0700

    Implemented insert_nodeint_at_index function

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position in a listint_t list.
+ * insert_nodeint_at_index - Inserts a new node in a listint_t list
  * @head: Pointer to the pointer of the head of the list.
  * @idx: Index where the new node should be added. Index starts at 0.
  * @n: Value to be added to the new node.
