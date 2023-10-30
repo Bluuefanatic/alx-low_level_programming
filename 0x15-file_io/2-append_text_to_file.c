729c3db1f287fc59eb0f8170c88f96e1eea0da8d
Joel Iziren <izirenjoel@gmail.com>
2023-10-30 06:02:40 -0700

    implemented append_text_to_file function

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int append_text_to_file(const char *filename, char *text_content) {
+	int fd;
+
+    if (filename == NULL)
+        return -1;
+
+    fd = open(filename, O_WRONLY | O_APPEND);
+
+    if (fd == -1)
+        return -1;
+
+    if (text_content != NULL) {
+        ssize_t bytesWritten = write(fd, text_content, strlen(text_content));
+
+        if (bytesWritten == -1) {
+            close(fd);
+            return -1;
+        }
+    }
+
+    close(fd);
+    return 1;
+}
