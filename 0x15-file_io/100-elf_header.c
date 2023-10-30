a4414fb95ce76452453f8e15996daafa5063fd2c
Joel Iziren <izirenjoel@gmail.com>
2023-10-30 09:25:28 -0700

    implemented ELF header information display

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -69,18 +69,11 @@ void print_elf_header_info(Elf64_Ehdr *header)
 	}
 	printf("\n");
 
-	printf("  Class:
-			%s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:
-			%s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:
-			%d (current)\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:
-			%s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
-	printf("  ABI Version:
-			%d\n", header->e_ident[EI_ABIVERSION]);
-	printf("  Type:
-			%s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "Other");
-	printf("  Entry point address:
-			%#lx\n", (unsigned long)header->e_entry);
+    printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+    printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
+    printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+    printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_SYSV ? "UNIX - System V" : "Other");
+    printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+    printf("  Type:                              %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "Other");
+    printf("  Entry point address:               %#lx\n", (unsigned long)header->e_entry);
 }
