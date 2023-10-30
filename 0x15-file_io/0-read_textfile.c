31a45a5a42d6de9e78a325db1d4ed0d7293f0def
Joel Iziren <izirenjoel@gmail.com>
2023-10-30 05:03:09 -0700

    implemented read_textfile function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,6 +2,12 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: Name of the file to be read
+ * @letters: Number of letters to read and print
+ * Return: The actual number of letters read and printed. 0 on failure.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
@@ -27,7 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		close(fd);
-		return 0;
+		return (0);
 	}
 
 	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
