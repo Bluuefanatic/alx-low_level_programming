3755808f4b333457fc054b013ca53629e75d6508
Joel Iziren <izirenjoel@gmail.com>
2023-12-11 04:46:36 -0800

    Create dynamic library for Python integration

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function to subtract two integers
+int subtract(int a, int b) {
+    return a - b;
+}
+
+// Function to multiply two integers
+int multiply(int a, int b) {
+    return a * b;
+}
+
+// Function to divide two integers
+int divide(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Error: Division by zero!\n");
+        return 0;
+    }
+}
