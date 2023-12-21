ad9c070b0bf09c62a2c86edc6d9bc3e3c6fd7517
Bluuefanatic <izirenjoel@gmail.com>
2023-12-21 15:44:19 -0800

    Add key_index function

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -9,11 +9,11 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash_value;
+	unsigned long int hash_value;
 
-    /* Get the hash value using the djb2 hash function */
-    hash_value = hash_djb2(key);
+	/* Get the hash value using the djb2 hash function */
+	hash_value = hash_djb2(key);
 
-    /* Return the index at which the key should be stored */
-    return (hash_value % size);
+	/* Return the index at which the key should be stored */
+	return (hash_value % size);
 }
