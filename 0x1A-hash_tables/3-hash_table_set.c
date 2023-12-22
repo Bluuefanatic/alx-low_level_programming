afae8d49351832978bca2a9f2d398a46b6d0bcd5
Bluuefanatic <izirenjoel@gmail.com>
2023-12-21 16:05:14 -0800

    Add hash_table_set function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,49 +12,63 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *new_node, *current_node;
-    unsigned long int index;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (0);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
 
-    /* Get the index for the key using the key_index function */
-    index = key_index((const unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 
-    /* Check if the key already exists in the linked list at the index */
-    current_node = ht->array[index];
-    while (current_node != NULL)
-    {
-        /* If the key exists, update the value and return success */
-        if (strcmp(current_node->key, key) == 0)
-        {
-            free(current_node->value);
-            current_node->value = strdup(value);
-            if (current_node->value == NULL)
-                return (0);
-            return (1);
-        }
-        current_node = current_node->next;
-    }
+	return (add_node_at_index(&(ht->array[index]), key, value));
+}
 
-    /* Key doesn't exist, create a new node and add it to the linked list */
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return (0);
+/**
+ * add_node_at_index - Adds a node at the beginning of a linked list
+ * @head: Pointer to the head of the linked list
+ * @key: The key
+ * @value: The value associated with the key
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+int add_node_at_index(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new_node;
 
-    new_node->key = strdup(key);
-    new_node->value = strdup(value);
-    if (new_node->key == NULL || new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node->value);
-        free(new_node);
-        return (0);
-    }
+	new_node = create_node(key, value);
+	if (new_node == NULL)
+		return (0);
 
-    /* Add the new node at the beginning of the linked list */
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new_node->next = *head;
+	*head = new_node;
 
-    return (1);
+	return (1);
+}
+
+/**
+ * create_node - Creates a new hash node
+ * @key: The key
+ * @value: The value associated with the key
+ *
+ * Return: A pointer to the new node, or NULL if an error occurs
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (NULL);
+	}
+
+	return (new_node);
 }
