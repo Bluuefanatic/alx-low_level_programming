09570cb0803e21fb3e2bf51f6ac8e3b4a526f0d7
Bluuefanatic <izirenjoel@gmail.com>
2023-12-21 17:35:35 -0800

    Add hash_table_set function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,74 +1,57 @@
+#include "hash_tables.h"
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include "hash_tables.h"
+
 
 /**
- * hash_table_set - Adds an element to the hash table
- * @ht: The hash table
- * @key: The key
- * @value: The value associated with the key
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
  *
- * Return: 1 if successful, 0 otherwise
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	return (add_node_at_index(&(ht->array[index]), key, value));
-}
-
-/**
- * add_node_at_index - Adds a node at the beginning of a linked list
- * @head: Pointer to the head of the linked list
- * @key: The key
- * @value: The value associated with the key
- *
- * Return: 1 if successful, 0 otherwise
- */
-int add_node_at_index(hash_node_t **head, const char *key, const char *value)
-{
-	hash_node_t *new_node;
-
-	new_node = create_node(key, value);
-	if (new_node == NULL)
-		return (0);
-
-	new_node->next = *head;
-	*head = new_node;
-
-	return (1);
-}
-
-/**
- * create_node - Creates a new hash node
- * @key: The key
- * @value: The value associated with the key
- *
- * Return: A pointer to the new node, or NULL if an error occurs
- */
-hash_node_t *create_node(const char *key, const char *value)
-{
-	hash_node_t *new_node;
-
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	if (new_node->key == NULL || new_node->value == NULL)
+	for (i = index; ht->array[i]; i++)
 	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
-		return (NULL);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
 	}
 
-	return (new_node);
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
 }
